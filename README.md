4808d40c77401f5272bad068ea2c3f9e1787e41a
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-10 16:10:28 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Webpack Basics
+
+This project is a simple implementation what I I learned about the basics of Webpack.
+
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import { greeting } from "./greeting.js";
+import "../css/styles.css";
+import odinImage from "../assets/odin.png";
+
+console.log(greeting); 
+
+const image = document.createElement("img");
+image.src = odinImage;
+document.body.appendChild(image);
