001e396a8f2108ef396c14ff79504b43afc5889f
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-10 22:28:00 +0530

    made some changed

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import { greeting } from "./greeting.js";
 import "../css/styles.css";
 import odinImage from "../assets/odin.png";
 
-console.log(greeting); 
+console.log(greeting);
 
 const image = document.createElement("img");
 image.src = odinImage;
